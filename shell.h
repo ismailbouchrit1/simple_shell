1cf71d59ae4b90fe0a732103d0270df92a22fe18
Ismail Bouchrit <ismail.bouchrit.ac@gmail.com>
2023-05-24 17:38:04 -0700

    update

--- shell.h
+++ shell.h
@@ -16,47 +16,72 @@
 extern char **environ;
 extern __sighandler_t signal(int __sig, __sighandler_t __handler);
 
-/* handle built ins */
-int checker(char **cmd, char *buf);
-void prompt_user(void);
-void handle_signal(int m);
+
+/* builtins.c */
 char **tokenizer(char *line);
-char *test_path(char **path, char *command);
-char *append_path(char *path, char *command);
 int handle_builtin(char **command, char *line);
+void handle_signal(int m);
+int checker(char **cmd, char *buf);
+
+
+/* execute.c */
+void execution(char *cp, char **cmd);
 void exit_cmd(char **command, char *line);
 
+/* free_fct.c */
+void free_buffers(char **buf);
+
+
+/*  path_handler.c  */
+char *find_path(void);
+char *append_path(char *path, char *command);
+char *test_path(char **path, char *command);
+
+/* print.c  */
+void prompt_user(void);
+int _putchar(char c);
+int print_s(char *s);
 void print_env(void);
 
-/* string handlers */
+
+/* str_handler.c  */
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
 int _strncmp(char *s1, char *s2, int n);
 char *_strdup(char *s);
 char *_strchr(char *s, char c);
 
-void execution(char *cp, char **cmd);
-char *find_path(void);
-char *test_path(char **path, char *command);
 
-/* helper function for efficient free */
-void free_buffers(char **buf);
-
+/**
+ * struct builtin - singly linked list
+ * @env: environment
+ * @exit: exit
+ */
 struct builtin
 {
 	char *env;
 	char *exit;
 } builtin;
 
+/**
+ * struct info - singly linked list
+ * @final_exit: ----
+ * @ln_count: ----
+ */
 struct info
 {
 	int final_exit;
 	int ln_count;
 } info;
 
+
+/**
+ * struct flags - singly linked list
+ * @interactive: --
+ */
 struct flags
 {
 	bool interactive;
 } flags;
 
-#endif /* SHELL_H */
+#endif
